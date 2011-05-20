56495ee668c8cf44df0fc805f0d1457b7998240e
Trey Smith <trey.smith@nasa.gov>
2011-05-20 07:12:18 -0700

    blessed geocamPycroraptor into an installable module

---
+++ geocamPycroraptor/tests.py
@@ -0,0 +1,15 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+from django.test import TestCase
+
+
+class geocamPycroraptorTest(TestCase):
+    """
+    Tests for geocamPycroraptor
+    """
+    def test_geocamPycroraptor(self):
+        pass
